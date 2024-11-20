ec9961bbe9e7f39c3548e57c8b5f51ceef2aec08
Breno Santos <brenovinicius.santos0@gmail.com>
2024-11-20 15:03:00 -0300

    Working with Predicate

---
+++ src/entities/Product.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Product {
+
+	private String name;
+	private double price;
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100;
+	}
+
+	public boolean nonStaticProductPredicate() {
+		return price >= 100;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+
+}
